7cd604b4eb401f8861d405ea6279bbe924e3b5d2
SergeyMoshkov <moshkov_s@vk.com>
2023-11-13 09:37:26 +0300

    first commit

---
+++ web/src/components/header/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="header flex justify-center items-center p-3 shadow-slate-400 rounded">
+      <h1 className="text-3xl m-3 font-bold text-slate-700">
+        <a href="/">IT Library</a>
+      </h1>
+      <p className="text-3xl m-3 font-bold text-slate-700">IT Library2</p>
+    </div>
+  );
+}
